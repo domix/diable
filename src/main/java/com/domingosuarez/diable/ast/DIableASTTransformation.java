50401652791c8f26042d8289735f36018ecc9587
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-06-10 23:40:05 -0500

    fixed ast

--- src/main/java/com/domingosuarez/diable/ast/DIableASTTransformation.java
+++ src/main/java/com/domingosuarez/diable/ast/DIableASTTransformation.java
@@ -115,8 +115,7 @@ public class DIableASTTransformation extends AbstractASTTransformation {
 
   private static void addNonThreadSafeBody(BlockStatement body, FieldNode fieldNode, Expression initExpr) {
     final Expression fieldExpr = varX(fieldNode);
-    //body.addStatement(ifElseS(notNullX(fieldExpr), stmt(fieldExpr), assignS(fieldExpr, initExpr)));
-    body.addStatement(assignS(fieldExpr, initExpr));
+    body.addStatement(ifElseS(notNullX(fieldExpr), stmt(fieldExpr), assignS(fieldExpr, initExpr)));
   }
 
   private static void addMethod(FieldNode fieldNode, BlockStatement body, ClassNode type) {
